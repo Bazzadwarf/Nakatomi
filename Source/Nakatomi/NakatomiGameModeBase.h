b734fc529b4eea1d4dbc04079b3b0dc9954df97d
Louis Hobbs <bazzadobs@live.co.uk>
2022-12-08 02:40:24 +0000

    Create empty UE5.1 project

---
+++ Source/Nakatomi/NakatomiGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NakatomiGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NAKATOMI_API ANakatomiGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};

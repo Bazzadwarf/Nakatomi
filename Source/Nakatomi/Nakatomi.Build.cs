f2824be7b9c6502b653c115c3164d7702c23ca7b
Louis Hobbs <bazzadobs@live.co.uk>
2023-01-31 21:35:27 +0000

    Add `FieldSystemEngine` and `GeometryCollectionEngine` to build file

--- Source/Nakatomi/Nakatomi.Build.cs
+++ Source/Nakatomi/Nakatomi.Build.cs
@@ -8,7 +8,7 @@ public class Nakatomi : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "FieldSystemEngine", "GeometryCollectionEngine" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 

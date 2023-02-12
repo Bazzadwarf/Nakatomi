e83659123ec868730ed1bcb2c611a6c5b2692428
Louis Hobbs <bazzadobs@live.co.uk>
2023-02-12 20:26:22 +0000

    Add `RandomWeapon` Class

---
+++ Source/Nakatomi/RandomWeapon.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RandomWeapon.h"
+
+ARandomWeapon::ARandomWeapon()
+{
+	RandomWeaponParameters = CreateDefaultSubobject<URandomWeaponParameters>(TEXT("Random Weapon Parameters"));
+}
+
+void ARandomWeapon::BeginPlay()
+{
+	WeaponProperties = RandomWeaponParameters->GenerateRandomWeaponProperties();
+	WeaponSkeletalMesh = RandomWeaponParameters->PickRandomMesh();
+	FireSound = RandomWeaponParameters->PickRandomSoundBase();
+	FieldSystemActor = RandomWeaponParameters->PickRandomFieldSystem();
+	
+	Super::BeginPlay();
+}
---
+++ Source/Nakatomi/RandomWeapon.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RandomWeaponParameters.h"
+#include "Weapon.h"
+#include "RandomWeapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NAKATOMI_API ARandomWeapon : public AWeapon
+{
+	GENERATED_BODY()
+	
+private: 
+
+	UPROPERTY(VisibleAnywhere, Category = "Random Weapon Parameters")
+	URandomWeaponParameters* RandomWeaponParameters;
+
+public:
+
+	ARandomWeapon();
+
+protected:
+	virtual void BeginPlay() override;
+};

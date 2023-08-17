763c8db97eca93d62375fdea1a0c3b9fbaf1cfe6
Louis Hobbs <bazzadobs@live.co.uk>
2023-08-17 23:05:59 +0100

    Create AcquireAttackToken task

---
+++ Source/Nakatomi/TaskAcquireAttackToken.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TaskAcquireAttackToken.h"
+
+EBTNodeResult::Type UTaskAcquireAttackToken::ExecuteTask(UBehaviorTreeComponent& owner, uint8* memory)
+{
+	return EBTNodeResult::Succeeded;
+}
---
+++ Source/Nakatomi/TaskAcquireAttackToken.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "TaskAcquireAttackToken.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NAKATOMI_API UTaskAcquireAttackToken : public UBTTaskNode
+{
+	GENERATED_BODY()
+
+public:
+
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& owner, uint8* memory) override;
+	
+};

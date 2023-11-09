357e4fdf5ea5d063cdf646c2788b2d3ecf953576
baz <bazzadobs@live.co.uk>
2023-11-09 22:14:45 +0000

    Create MainMenuGameModeBase

---
+++ Source/Nakatomi/MainMenuGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MainMenuGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NAKATOMI_API AMainMenuGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};

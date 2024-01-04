56aa855a7c013405509aa8d47de35f1910e3f477
baz <bazzadobs@live.co.uk>
2024-01-04 15:49:09 +0000

    Create NakatomiCMC

---
+++ Source/Nakatomi/NakatomiCMC.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "NakatomiCMC.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NAKATOMI_API UNakatomiCMC : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+	
+};

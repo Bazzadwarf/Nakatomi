a3120e27f30810e5db6358963aa4ce1b8b56e4b3
baz <bazzadobs@live.co.uk>
2024-05-21 22:06:57 +0100

    Create NakatomiSaveGame class

--- Source/Nakatomi/NakatomiSaveGame.h
+++ Source/Nakatomi/NakatomiSaveGame.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Weapon.h"
 #include "GameFramework/SaveGame.h"
 #include "NakatomiSaveGame.generated.h"
 
@@ -13,5 +14,26 @@ UCLASS()
 class NAKATOMI_API UNakatomiSaveGame : public USaveGame
 {
 	GENERATED_BODY()
+
+public:
+	UPROPERTY(VisibleAnywhere, Category = Basic)
+	FString PlayerName = "Player Name";
+
+	UPROPERTY(VisibleAnywhere, Category = Level)
+	FString LevelName = "Level1";
+
+	UPROPERTY(VisibleAnywhere, Category = Player)
+	float PlayerHealth = 100.0f;
+
+	UPROPERTY(VisibleAnywhere, Category = Player)
+	TArray<AWeapon*> WeaponInventory;
+
+	UPROPERTY(VisibleAnywhere, Category = Player)
+	int CurrentInventorySlot = 0;
 	
+	UFUNCTION(BlueprintCallable)
+	void ResetPlayerValuesToDefault()
+	{
+		PlayerHealth = 100.0f;
+	}
 };
